cec58dafd6e7eee6e59bd56c139629c89b8f36bb
ThomasEichy <ThomasEichy@live.com>
2020-02-28 21:27:38 -0600

    Cheese Project Finished

---
+++ Controllers/CheeseController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class_4_Exercise.Controllers
+{
+    public class CheeseController : Controller
+    {
+
+        static private Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+
+        public IActionResult Index()
+        {
+            ViewBag.cheeses = Cheeses;
+
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Cheese/Add")]
+        public IActionResult NewCheese(string name, string description)
+        {
+            Cheeses.Add(name, description);
+
+            return Redirect("/Cheese");
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Class_4_Exercise.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
